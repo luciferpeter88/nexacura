14cc83bb48a519ac758182f0ebef02ee8fd9b3fa
luciferpeter88 <kaszapnagyp@gmail.com>
2024-04-07 13:35:58 +0100

    .

--- nexacura-frontend/src/Components/Avatar/config.js
+++ nexacura-frontend/src/Components/Avatar/config.js
@@ -5,13 +5,12 @@ ICE server URL, ICE server username, and ICE server credential. By exporting thi
 imported and used in other parts of the application where these configuration settings are required. */
 export const avatarAppConfig = {
   cogSvcRegion: "westeurope",
-  cogSvcSubKey: "cd724c1fe2924a5e9c871802a191fe9f",
+  cogSvcSubKey: "YOUR_COGSVC_SUBSCRIPTION_KEY",
   voiceName: "en-GB-LibbyNeural",
   avatarCharacter: "lisa",
   avatarStyle: "casual-sitting",
   avatarBackgroundColor: "#FFFFFFFF",
   iceUrl: "stun:relay.communication.microsoft.com:3478",
-  iceUsername:
-    "BQAANu2wYQAB2kbnZU/5QD320YHDI7jXbxzRj2cPJa0AAAAMARD/wiGWK4tL3rH7HFLHQFUO9tTR/a7I2ReLZO0+jYVKQHZC1O4=",
-  iceCredential: "lSUJoxOZ9oNoYAkV3azLCFaW22Q=",
+  iceUsername: "YOUR_ICE_USERNAME",
+  iceCredential: "YOUR_ICE_CREDENTIAL",
 };
